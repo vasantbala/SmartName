f82b200bb41501fd30bb18372016dae3d2acc9b2
Vasant Balasubramanian <vasantbala@outlook.com>
2019-12-11 16:25:21 -0600

    Used Creation date to check if the bing image was downloaded for the day

--- SmartNamePlate.Web/Controllers/ImageController.cs
+++ SmartNamePlate.Web/Controllers/ImageController.cs
@@ -26,14 +26,16 @@ namespace SmartNamePlate.Web.Controllers
 				System.IO.FileInfo fileInfo = new System.IO.FileInfo(bingImagePath);
 				
                 if (System.IO.File.Exists(bingImagePath) == false
-					|| fileInfo.LastWriteTime.Date < DateTime.Now.Date)
+					|| fileInfo.CreationTime.Date < DateTime.Now.Date)
                 {
+					System.IO.File.Delete(bingImagePath);
+
                     string bingImage = await Utils.GetAsync("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1");
                     dynamic json = JsonConvert.DeserializeObject<ExpandoObject>(bingImage);
                     string imgUrl = System.IO.Path.Join("https://www.bing.com", json.images[0].url);
                     byte[] imgBytes = await Utils.GetBytesAsync(imgUrl);
                     await System.IO.File.WriteAllBytesAsync(bingImagePath, imgBytes);
-                    System.IO.File.SetLastWriteTime(bingImagePath, DateTime.Now.AddDays(-1));
+                    //System.IO.File.SetLastWriteTime(bingImagePath, DateTime.Now.AddDays(-1));
                 }
             }
             catch
